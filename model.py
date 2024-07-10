9963e52421fff5b7360ba145b57e53be8747c904
Mark Sass <msass10@yahoo.com>
2024-07-10 19:47:56 -0400

    second commit

--- model.py
+++ model.py
@@ -13,6 +13,8 @@ class User(db.Model):
    password = db.Column(db.String)
    hiking_experience = db.Column(db.String)
 
+   
+
 
 
 class Trails(db.Model):
@@ -31,19 +33,23 @@ class Trails(db.Model):
    difficulty = db.Column(db.Integer)
    route_type = db.Column(db.String)
 
+   training_trails= db.relationship("Training_trails", back_populates="trails")
 
 
-class Training_Path(db.Model):
+class Training_path(db.Model):
     """user training path"""
 
     __tablename__ = "training_path"
 
-    Training_Path_id =db.Column(db.Integer, autoincrement=True, primary_key=True)
+    training_path_id =db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     summit_goal = db.Column(db.String)
     training_trails = db.Column(db.String)
-    trails_completed = db.Column(db.String)
+    training_path_completed = db.Column(db.Boolean)
     progress_mileage = db.Column(db.Float)
+
+    activities = db.relationship("Activity_log", back_populates="training_path")
+    
 
 
 class Activity_log(db.Model):
@@ -51,13 +57,27 @@ class Activity_log(db.Model):
 
     __tablename__ = "activities"
 
-    training_path_id = db.Column(db.Integer, db.ForeignKey("training_path.training_path-id"))
+    activity_log_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    training_path_id = db.Column(db.Integer, db.ForeignKey("training_path.training_path_id"))
     mileage_log = db.Column(db.Integer)
     sub_summit_trails = db.Column(db.String)
     trail_notes = db.Column(db.Text)
     trail_id = db.Column(db.Integer, db.ForeignKey("trails.trail_id"))
 
+    training_path = db.relationship("Training_path", back_populates="activities")
 
+
+class Training_trails(db.Model):
+    """training trails"""
+
+    __tablename__ = "training_trails"
+
+    training_trail_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    trail_id = db.Column(db.Integer, db.ForeignKey("trails.trail_id"))
+    training_path_id = db.Column(db.Integer, db.ForeignKey("training_path.training_path_id"))
+    trails_completed = db.Column(db.Boolean)
+
+    trails = db.relationship("Trails" , back_populates="training_trails")
 
 
 
